2a51dbb0f080e2c248a76764b0e3d99cf2c3cd7d
frontrangerider <frontrangerider2004@gmail.com>
2022-02-22 08:52:38 -0700

    refactor(stringNumberSum): eliminate if-else
    Use modulo and division to eliminate the need for if-else block
    computing the carry and sum values.

--- src/stacks/string-number-sum.js
+++ src/stacks/string-number-sum.js
@@ -1,15 +1,21 @@
 /**
- * Computes the sum of two large numbers represented as string
+ * Computes the sum of two large numbers represented as string using a Stack
+ * and a queue represented by ES6 Arrays. This will push each letter of the
+ * input strings onto two stacks and then pop() and sum into a queue.
+ *
+ * SIMILAR PROBLEMS:
+ * Given two LinkedList of numbers add them together
+ *
  * @param {string} a large number as a string
  * @param {string} b large number as a string
- * @return {string} the sum of a and b
+ * @return {string} the sum of a and b, digit by digit
  *
  * @example
  * '1' + '2' = '3'
  * '11' + '22' = '33'
  * '15' +
  */
-function largeNumberSum(a = '', b = '') {
+function stringNumberSum(a = '', b = '') {
   if (a.length === 0 && b.length === 0) {
     return undefined;
   } else if (a.length === 0) {
@@ -18,41 +24,25 @@ function largeNumberSum(a = '', b = '') {
     return a;
   }
 
-  // convert these into 'stacks'
   const results = [];
   const aDigits = a.split('');
   const bDigits = b.split('');
 
-  // treat this like a stack since es6 arrays stack-like
   let aDigit = aDigits.pop();
   let bDigit = bDigits.pop();
   let carry = 0;
 
-  // we can use truthyness since string '0' is truthy and
-  // if carry = 0 that's in integer which is falsey so we don't continue
   while (aDigit || bDigit || carry) {
-    // ensure we handle any leftover digits
     let sum = carry;
 
-    // have to parseInt because a and b are strings
     sum = aDigit ? sum + parseInt(aDigit) : sum;
     sum = bDigit ? sum + parseInt(bDigit) : sum;
 
-    let result = sum;
-    if (sum > 9) {
-      // need to carry the 10s and record the 1s place
-      result = sum - 10;
-      carry = 1;
-    } else {
-      // reset carry otherwise loop will continue infinitely
-      carry = 0;
-    }
+    const result = sum % 10;
+    carry = parseInt(sum / 10);
 
-    // cast back to string and insert from left to right like a queue
-    // since we are computing from right to left
     results.unshift(String(result));
 
-    // change the loop conditions so we don't infinitely loop
     aDigit = aDigits.pop();
     bDigit = bDigits.pop();
   }
@@ -60,4 +50,56 @@ function largeNumberSum(a = '', b = '') {
   return results.join('');
 }
 
-module.exports = largeNumberSum;
+module.exports = stringNumberSum;
+
+// Alternate
+// function stringNumberSum(a = '', b = '') {
+//   if (a.length === 0 && b.length === 0) {
+//     return undefined;
+//   } else if (a.length === 0) {
+//     return b;
+//   } else if (b.length === 0) {
+//     return a;
+//   }
+
+//   // convert these into 'stacks'
+//   const results = [];
+//   const aDigits = a.split('');
+//   const bDigits = b.split('');
+
+//   // treat this like a stack since es6 arrays stack-like
+//   let aDigit = aDigits.pop();
+//   let bDigit = bDigits.pop();
+//   let carry = 0;
+
+//   // we can use truthyness since string '0' is truthy and
+//   // if carry = 0 that's in integer which is falsey so we don't continue
+//   while (aDigit || bDigit || carry) {
+//     // ensure we handle any leftover digits
+//     let sum = carry;
+
+//     // have to parseInt because a and b are strings
+//     sum = aDigit ? sum + parseInt(aDigit) : sum;
+//     sum = bDigit ? sum + parseInt(bDigit) : sum;
+
+//     let result = sum;
+//     if (sum > 9) {
+//       // need to carry the 10s and record the 1s place
+//       result = sum - 10;
+//       carry = 1;
+//     } else {
+//       // reset carry otherwise loop will continue infinitely
+//       carry = 0;
+//     }
+
+//     // cast back to string and insert from left to right like a queue
+//     // since we are computing from right to left
+//     results.unshift(String(result));
+
+//     // change the loop conditions so we don't infinitely loop
+//     aDigit = aDigits.pop();
+//     bDigit = bDigits.pop();
+//   }
+
+//   return results.join('');
+// }
--- test/string-number-sum.test.js
+++ test/string-number-sum.test.js
@@ -1,41 +1,45 @@
-const largeNumberSum = require('./index');
+const stringNumberSum = require('../src/stacks/string-number-sum');
 
 test('largeNumberSum is a function', () => {
-  expect(typeof largeNumberSum).toEqual('function');
+  expect(typeof stringNumberSum).toEqual('function');
 });
 
 test('it returns undefined when both a and b are undefined', () => {
-  expect(largeNumberSum()).toEqual(undefined);
+  expect(stringNumberSum()).toEqual(undefined);
 });
 
 test('it returns a when only a is defined', ()=> {
-  expect(largeNumberSum('15', undefined)).toEqual('15');
+  expect(stringNumberSum('15', undefined)).toEqual('15');
 });
 
 test('it returns b when only b is defined', ()=> {
-  expect(largeNumberSum(undefined, '7')).toEqual('7');
+  expect(stringNumberSum(undefined, '7')).toEqual('7');
 });
 
 test('it returns undefined when both a and b are zero length', ()=> {
-  expect(largeNumberSum('', '')).toEqual(undefined);
+  expect(stringNumberSum('', '')).toEqual(undefined);
 });
 
 test('it returns a when b is zero length', ()=> {
-  expect(largeNumberSum('6', '')).toEqual('6');
+  expect(stringNumberSum('6', '')).toEqual('6');
 });
 
 test('it returns b when a is zero length', ()=> {
-  expect(largeNumberSum('', '9')).toEqual('9');
+  expect(stringNumberSum('', '9')).toEqual('9');
 });
 
 test('1 + 2 = 3', ()=> {
-  expect(largeNumberSum('1', '2')).toEqual('3');
+  expect(stringNumberSum('1', '2')).toEqual('3');
 });
 
 test('12 + 34 = 46', ()=> {
-  expect(largeNumberSum('12', '34')).toEqual('46');
+  expect(stringNumberSum('12', '34')).toEqual('46');
 });
 
 test('57 + 75 = 132', ()=> {
-  expect(largeNumberSum('57', '75')).toEqual('132');
+  expect(stringNumberSum('57', '75')).toEqual('132');
+});
+
+test('99 + 99 = 198', ()=> {
+  expect(stringNumberSum('99', '99')).toEqual('198');
 });
